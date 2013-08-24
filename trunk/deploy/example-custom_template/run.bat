2525890cb7b840a8f174f8e0388f20602b21cc94
askldjd@gmail.com <askldjd@gmail.com@d477edad-9946-b3fb-2e57-acbd60b361e5>
2013-08-24 09:39:51 +0000

    Updated all the run.bat for the latest update.

--- trunk/deploy/example-custom_template/run.bat
+++ trunk/deploy/example-custom_template/run.bat
@@ -29,7 +29,8 @@ cmd /c %fop_path%\fop.bat ^
 -xsl %fop_path%\docbook-xsl-1.78.0\fo\docbook.xsl ^
 -pdf %pdf_output% ^
 -param page.orientation landscape ^
--param paper.type USletter
+-param paper.type USletter ^
+-param body.font.family sans-serif
 
 :concat_files
 set proto_files=%proto_files% %1
